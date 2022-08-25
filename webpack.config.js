4a994a31f7fc4bff5d37689c6c1062b76c2a5c35
EdricY <EdricY@users.noreply.github.com>
2022-08-25 16:20:27 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./src/main.js",
   output: {
     path: path.resolve("dist"),
-    filename: "game.js",
+    filename: "main.js",
     publicPath: "/dist/",
   },
   resolve: {
